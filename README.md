1e977c3a4da138a1446cb3aa1d9aadd702adf183
Dimitri Labouesse <dlabouesse@live.fr>
2018-11-09 10:57:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Introduction
+The purpose of this script is to automatically install the following nodes of Dataiku in one single command:
+- Design node
+- Automation node
+- API node
+
+# How to use?
+This script accepts two input options:
+- **Dataiku installer**
+The path to the Dataiku installer in the `.tar.gz` format must be set using the `-f` option.
+`./dku-installer.sh -f dataiku-dss-5.0.5-osx.tar.gz`
+
+- **License file (optional)**
+The path to the licence file can be set using the `-l` option.
+`./dku-installer.sh -f dataiku-dss-5.0.5-osx.tar.gz -l license.json`
+
+# Running details
+## Directories
+This script creates a folder depending of the version of Dataiku you want to install, that will respects the following schema `DSS_$VERSION` (e.g `DSS_5_0_3`).
+Then, it will extract the installer in this folder, and install the 3 nodes using the following `DATA_DIR` directories:
+- Design node: `/DSS_$VERSION/dss_home`
+- Automation node: `/DSS_$VERSION/dss_home_automation`
+- API node: `/DSS_$VERSION/dss_home_apinode`
+
+## Ports
+Each node is installed on a port depending of the version of DSS and which is suffixed by the following number depending of the node:
+- Design node: `00` (e.g `50300`)
+- Automation node: `10` (e.g `50310`)
+- API node: `20` (e.g `50320`)
---
+++ dku-installer.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+while getopts ":f:l:" option
+do
+    case $option in
+        f)
+            DKUINSTALLER=$OPTARG
+            ;;
+        l)
+            LICENSE=$OPTARG
+            ;;
+        :)
+            echo "The option -$OPTARG requires an argument"
+            exit 1
+            ;;
+        \?)
+            echo "-$OPTARG : invalid option"
+            exit 1
+            ;;
+    esac
+done
+
+if [ -z "$DKUINSTALLER" ]
+then
+    echo "Missing path to Dataiku installer (please use the -f option)"
+    exit 1
+fi
+
+VERSION=$(echo $DKUINSTALLER | cut -d'-' -f 3)
+
+read -p "Are you sure you want to install dataiku $VERSION? " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo "Install aborted"
+    exit 0
+fi
+echo
+echo "Installing Dataiku $VERSION..."
+DKUDIR="DSS_$(echo $VERSION | tr . _)"
+echo "Creating $DKUDIR folder..."
+mkdir $DKUDIR
+echo "Extracting $DKUINSTALLER..."
+tar zxf $DKUINSTALLER -C ./$DKUDIR
+cd $DKUDIR
+DKUINSTALLER=$(ls)
+echo $DKUINSTALLER
+
+PORT_DESIGN="$(echo "$VERSION" | tr -d .)00"
+PORT_AUTOMATION="$(echo "$VERSION" | tr -d .)10"
+PORT_API="$(echo "$VERSION" | tr -d .)20"
+
+if [ -z "$LICENSE" ]
+then
+    echo "Installing design node on port $PORT_DESIGN without license..."
+    $DKUINSTALLER/installer.sh -d dss_home -p $PORT_DESIGN
+
+    echo -e "\nInstalling automation node on port $PORT_AUTOMATION without license..."
+    $DKUINSTALLER/installer.sh -t automation -d dss_home_automation -p $PORT_AUTOMATION
+
+    echo -e "\nInstalling api node on port $PORT_API without license..."
+    $DKUINSTALLER/installer.sh -t api -d dss_home_apinode -p $PORT_API
+else
+    echo -e "Installing design node on port $PORT_DESIGN with license..."
+    $DKUINSTALLER/installer.sh -d dss_home -p $PORT_DESIGN -l ../$LICENSE
+    
+    echo -e "\nInstalling automation node on port $PORT_AUTOMATION with license..."
+    $DKUINSTALLER/installer.sh -t automation -d dss_home_automation -p $PORT_AUTOMATION -l ../$LICENSE
+
+    echo -e "\nInstalling api node on port $PORT_API with license..."
+    $DKUINSTALLER/installer.sh -t api -d dss_home_apinode -p $PORT_API -l ../$LICENSE
+fi
+
+echo -e "\nInstall completed!"
+echo "Design node: http://localhost:$PORT_DESIGN (./$DKUDIR/dss_home/bin/dss start)"
+echo "Automation node: http://localhost:$PORT_AUTOMATION (./$DKUDIR/dss_home_automation/bin/dss start)"
+echo "API node: http://localhost:$PORT_API (./$DKUDIR/dss_home_apinode/bin/dss start)"
+
+exit 0
